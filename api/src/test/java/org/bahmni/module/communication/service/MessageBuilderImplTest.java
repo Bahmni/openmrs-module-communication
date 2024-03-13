d5bce314323468d7bd040cd266c2c8b3e8d9e0ac
abhiyansh <abhiyansharya161@gmail.com>
2024-03-13 14:52:09 +0530

    BAH-3268 Add tests for MessageBuilderImpl

--- api/src/test/java/org/bahmni/module/communication/service/MessageBuilderImplTest.java
+++ api/src/test/java/org/bahmni/module/communication/service/MessageBuilderImplTest.java
@@ -2,54 +2,44 @@ package org.bahmni.module.communication.service;
 
 import org.bahmni.module.communication.service.impl.MessageBuilderServiceImpl;
 import org.bahmni.module.communication.service.impl.SMSTemplateService;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.powermock.modules.junit4.PowerMockRunner;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
 
 @RunWith(PowerMockRunner.class)
 public class MessageBuilderImplTest {
-	
-	@InjectMocks
-	MessageBuilderServiceImpl messageBuilderService;
-	
-	@Mock
-	SMSTemplateService smsTemplateService;
-	
-	@Before
-	public void setUp() {
-		MockitoAnnotations.initMocks(this);
-	}
-	
-	@Test
-	public void testGetRegistrationMessage() {
-		Map<String, String> arguments = Collections.singletonMap("key", "value");
-		String expectedTemplate = "Registration SMS Template";
-		
-		when(smsTemplateService.templateMessage(MessageBuilderServiceImpl.PATIENT_REGISTRATION_SMS_TEMPLATE, arguments))
-		        .thenReturn(expectedTemplate);
-		
-		String result = messageBuilderService.getRegistrationMessage(arguments);
-		
-		assertEquals(expectedTemplate, result);
-	}
-	
-	@Test
+
+    @InjectMocks
+    MessageBuilderServiceImpl messageBuilderService;
+
+    @Mock
+    SMSTemplateService smsTemplateService;
+
+    @Test
+    public void testGetRegistrationMessage() {
+        Map<String, String> arguments = Collections.singletonMap("key", "value");
+        String expectedTemplate = "Registration SMS Template";
+
+        when(smsTemplateService.templateMessage(MessageBuilderServiceImpl.PATIENT_REGISTRATION_SMS_TEMPLATE, arguments))
+                .thenReturn(expectedTemplate);
+
+        String result = messageBuilderService.getRegistrationMessage(arguments);
+
+        assertEquals(expectedTemplate, result);
+    }
+
+    @Test
     public void testGetAppointmentBookingMessage() {
         Map<String, String> arguments = new HashMap<>();
-        arguments.put("key","value");
-        arguments.put("appointmentKind","Virtual");
+        arguments.put("key", "value");
+        arguments.put("appointmentKind", "Virtual");
         List<String> providers = Collections.singletonList("Provider1");
         String expectedTemplate = "Appointment Booking SMS Template";
 
@@ -57,15 +47,15 @@ public class MessageBuilderImplTest {
                 .thenReturn(expectedTemplate);
 
         String result = messageBuilderService.getAppointmentBookingMessage(arguments, providers);
-        String expectedResult=expectedTemplate+"nullnullnull";
+        String expectedResult = expectedTemplate + "nullnullnull";
         assertEquals(expectedResult, result);
     }
-	
-	@Test
+
+    @Test
     public void testGetRecurringAppointmentBookingMessage() {
         Map<String, String> arguments = new HashMap<>();
-        arguments.put("key","value");
-        arguments.put("appointmentKind","Virtual");
+        arguments.put("key", "value");
+        arguments.put("appointmentKind", "Virtual");
         List<String> providers = Collections.singletonList("Provider1");
         String expectedTemplate = "Recurring Appointment Booking SMS Template";
 
@@ -74,15 +64,15 @@ public class MessageBuilderImplTest {
 
         String result = messageBuilderService.getRecurringAppointmentBookingMessage(arguments, providers);
 
-        String expectedResult=expectedTemplate+"nullnullnull";
+        String expectedResult = expectedTemplate + "nullnullnull";
         assertEquals(expectedResult, result);
     }
-	
-	@Test
+
+    @Test
     public void testGetAppointmentReminderMessage() {
         Map<String, String> arguments = new HashMap<>();
-        arguments.put("key","value");
-        arguments.put("appointmentKind","Virtual");
+        arguments.put("key", "value");
+        arguments.put("appointmentKind", "Virtual");
         List<String> providers = Collections.singletonList("Provider1");
         String expectedTemplate = "Appointment Reminder SMS Template";
 
@@ -91,15 +81,15 @@ public class MessageBuilderImplTest {
 
         String result = messageBuilderService.getAppointmentReminderMessage(arguments, providers);
 
-        String expectedResult=expectedTemplate+"nullnullnull";
+        String expectedResult = expectedTemplate + "nullnullnull";
         assertEquals(expectedResult, result);
     }
-	
-	@Test
+
+    @Test
     public void testGenerateAppointmentMessage() {
         Map<String, String> arguments = new HashMap<>();
-        arguments.put("key","value");
-        arguments.put("appointmentKind","Virtual");
+        arguments.put("key", "value");
+        arguments.put("appointmentKind", "Virtual");
         List<String> providers = Collections.singletonList("Provider1");
         String smsTemplate = "Appointment SMS Template";
         String expectedTemplate = "Generated Appointment SMS Template";
@@ -116,17 +106,58 @@ public class MessageBuilderImplTest {
 
         assertEquals("Generated Appointment SMS TemplateProvider TemplateTeleconsultation Link TemplateHelpdesk Template", result);
     }
-	
-	@Test
-	public void testGeneratePatientMessage() {
-		Map<String, String> arguments = Collections.singletonMap("key", "value");
-		String smsTemplate = "Patient SMS Template";
-		String expectedTemplate = "Generated Patient SMS Template";
-		
-		when(smsTemplateService.templateMessage(smsTemplate, arguments)).thenReturn(expectedTemplate);
-		
-		String result = messageBuilderService.generatePatientMessage(arguments, smsTemplate);
-		
-		assertEquals(expectedTemplate, result);
-	}
+
+    @Test
+    public void testGenerateAppointmentMessageWhenProviderIsEmpty() {
+        Map<String, String> arguments = new HashMap<>();
+        arguments.put("key", "value");
+        arguments.put("appointmentKind", "Virtual");
+        List<String> providers = new ArrayList<>();
+        String smsTemplate = "Appointment SMS Template";
+        String expectedTemplate = "Generated Appointment SMS Template";
+
+        when(smsTemplateService.templateMessage(smsTemplate, arguments)).thenReturn(expectedTemplate);
+        when(smsTemplateService.templateMessage(MessageBuilderServiceImpl.APPOINTMENT_TELECONSULTATION_LINK_TEMPLATE, arguments))
+                .thenReturn("Teleconsultation Link Template");
+        when(smsTemplateService.templateMessage(MessageBuilderServiceImpl.HELPDESK_TEMPLATE, arguments))
+                .thenReturn("Helpdesk Template");
+
+        String result = messageBuilderService.generateAppointmentMessage(arguments, providers, smsTemplate);
+
+        assertEquals("Generated Appointment SMS TemplateTeleconsultation Link TemplateHelpdesk Template", result);
+    }
+
+    @Test
+    public void testGenerateAppointmentMessageWhenAppointmentKindIsNotVirtual() {
+        Map<String, String> arguments = new HashMap<>();
+        arguments.put("key", "value");
+        arguments.put("appointmentKind", "InPerson");
+        List<String> providers = Collections.singletonList("Provider1");
+        String smsTemplate = "Appointment SMS Template";
+        String expectedTemplate = "Generated Appointment SMS Template";
+
+        when(smsTemplateService.templateMessage(smsTemplate, arguments)).thenReturn(expectedTemplate);
+        when(smsTemplateService.templateMessage(MessageBuilderServiceImpl.APPOINTMENT_PROVIDER_TEMPLATE, arguments))
+                .thenReturn("Provider Template");
+        when(smsTemplateService.templateMessage(MessageBuilderServiceImpl.HELPDESK_TEMPLATE, arguments))
+                .thenReturn("Helpdesk Template");
+
+        String result = messageBuilderService.generateAppointmentMessage(arguments, providers, smsTemplate);
+
+        assertEquals("Generated Appointment SMS TemplateProvider TemplateHelpdesk Template", result);
+    }
+
+
+    @Test
+    public void testGeneratePatientMessage() {
+        Map<String, String> arguments = Collections.singletonMap("key", "value");
+        String smsTemplate = "Patient SMS Template";
+        String expectedTemplate = "Generated Patient SMS Template";
+
+        when(smsTemplateService.templateMessage(smsTemplate, arguments)).thenReturn(expectedTemplate);
+
+        String result = messageBuilderService.generatePatientMessage(arguments, smsTemplate);
+
+        assertEquals(expectedTemplate, result);
+    }
 }
